38a0f0b0862e0a39be2d63401384f2a96a8225c8
od3ng <noprianto@polinema.ac.id>
2019-11-21 23:40:17 +0700

    tidak wait ketika nggak ada kartu

--- GET-SERIAL-ESP32-PN532-SPI.ino
+++ GET-SERIAL-ESP32-PN532-SPI.ino
@@ -26,12 +26,13 @@ const char* password = "lepengdados";
 
 // Add your MQTT Broker IP address, example:
 //const char* mqtt_server = "192.168.1.144";
-const char* mqtt_server = "192.168.8.102";
+const char* mqtt_server = "192.168.8.100";
 const char* ntp_server = "asia.pool.ntp.org";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 long lastMsg = 0;
+String serialNumber = "";
 
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, ntp_server);
@@ -56,6 +57,7 @@ void setup(void) {
   Serial.print('.'); Serial.println((versiondata >> 8) & 0xFF, DEC);
 
   // configure board to read RFID tags
+  nfc.setPassiveActivationRetries(0x01);
   nfc.SAMConfig();
 
   Serial.println("Waiting for an ISO14443A Card ...");
@@ -136,255 +138,249 @@ void loop(void) {
   // 'uid' will be populated with the UID, and uidLength will indicate
   // if the uid is 4 bytes (Mifare Classic) or 7 bytes (Mifare Ultralight)
   success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
-
   if (success) {
-    // Display some basic information about the card
-    Serial.println("Found an ISO14443A card");
-    Serial.print("  UID Length: "); Serial.print(uidLength, DEC); Serial.println(" bytes");
-    Serial.print("  UID Value: ");
-    nfc.PrintHex(uid, uidLength);
+    char strSerial[8] = "";
+    array_to_string(uid, 4, strSerial);
+    String cardSerial = String(strSerial);
+    Serial.printf("Serialnya memory %s", serialNumber);
+    Serial.println();
+    Serial.printf("Serialnya %s", cardSerial);
+    Serial.println();
+    if (!serialNumber.equals(cardSerial)) {
+      // Display some basic information about the card
 
-    if (uidLength == 4) {
-      // We probably have a Mifare Classic card ...
-      uint32_t cardid = uid[0];
-      cardid <<= 8;
-      cardid |= uid[1];
-      cardid <<= 8;
-      cardid |= uid[2];
-      cardid <<= 8;
-      cardid |= uid[3];
-      Serial.print("Seems to be a Mifare Classic card #");
-      Serial.println(cardid);
-      Serial.println("");
-      Serial.println("");
+      Serial.println("Found an ISO14443A card");
+      Serial.print("  UID Length: "); Serial.print(uidLength, DEC); Serial.println(" bytes");
+      Serial.print("  UID Value: ");
+      nfc.PrintHex(uid, uidLength);
 
-      // We probably have a Mifare Classic card ...
-      Serial.println("Seems to be a Mifare Classic card (4 byte UID)");
+      if (uidLength == 4) {
+        // We probably have a Mifare Classic card ...
+        uint32_t cardid = uid[0];
+        cardid <<= 8;
+        cardid |= uid[1];
+        cardid <<= 8;
+        cardid |= uid[2];
+        cardid <<= 8;
+        cardid |= uid[3];
+        Serial.print("Seems to be a Mifare Classic card #");
+        Serial.println(cardid);
+        Serial.println("");
+        Serial.println("");
 
-      // Now we need to try to authenticate it for read/write access
-      // Try with the factory default KeyA: 0xFF 0xFF 0xFF 0xFF 0xFF 0xFF
-      Serial.println("Trying to authenticate block 4 with default KEYA value");
-      uint8_t keya[6] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
+        // We probably have a Mifare Classic card ...
+        Serial.println("Seems to be a Mifare Classic card (4 byte UID)");
 
-      // Start with block 4 (the first block of sector 1) since sector 0
-      // contains the manufacturer data and it's probably better just
-      // to leave it alone unless you know what you're doing
-      success = nfc.mifareclassic_AuthenticateBlock(uid, uidLength, 4, 0, keya);
+        // Now we need to try to authenticate it for read/write access
+        // Try with the factory default KeyA: 0xFF 0xFF 0xFF 0xFF 0xFF 0xFF
+        Serial.println("Trying to authenticate block 4 with default KEYA value");
+        uint8_t keya[6] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
 
-      if (success)
-      {
-        Serial.println("Sector 1 (Blocks 4..7) has been authenticated");
-        uint8_t temp[48];
-
-        // If you want to write something to block 4 to test with, uncomment
-        // the following line and this text should be read back in a minute
-        // data = { 'a', 'd', 'a', 'f', 'r', 'u', 'i', 't', '.', 'c', 'o', 'm', 0, 0, 0, 0};
-        // success = nfc.mifareclassic_WriteDataBlock (4, data);
-
-        // Try to read the contents of block 4
-        //        success = nfc.mifareclassic_ReadDataBlock(4, data);
-        int index = 0;
-        for (int i = 4; i < 7; i++) {
-          uint8_t data[16];
-          success = nfc.mifareclassic_ReadDataBlock(i, data);
-
-          for (int j = 0; j < 16; j++) {
-            temp[index++] = data[j];
-          }
-        }
+        // Start with block 4 (the first block of sector 1) since sector 0
+        // contains the manufacturer data and it's probably better just
+        // to leave it alone unless you know what you're doing
+        success = nfc.mifareclassic_AuthenticateBlock(uid, uidLength, 4, 0, keya);
 
         if (success)
         {
-          unsigned long unixTime = timeClient.getEpochTime();
-          Serial.println(unixTime);
-          String hexDate =  String(unixTime, HEX);
-          hexDate.toUpperCase();
-          Serial.println(hexDate);
-          char arrDate[hexDate.length() + 1];
-          hexDate.toCharArray(arrDate, hexDate.length() + 1);
-          uint8_t tglByte[4];
-          uint8_t tglByteTemp[4] = {};
-          hex2ByteArray(arrDate, 4, tglByte);
-          copy(begin(tglByte), end(tglByte), tglByteTemp);
-          nfc.PrintHexChar(tglByte, 4);
-          nfc.PrintHexChar(tglByteTemp, 4);
-          // Data seems to have been read ... spit it out
-          Serial.println("Reading Block 4:");
-          nfc.PrintHexChar(temp, 48);
+          Serial.println("Sector 1 (Blocks 4..7) has been authenticated");
+          uint8_t temp[48] = {};
 
+          // If you want to write something to block 4 to test with, uncomment
+          // the following line and this text should be read back in a minute
+          // data = { 'a', 'd', 'a', 'f', 'r', 'u', 'i', 't', '.', 'c', 'o', 'm', 0, 0, 0, 0};
+          // success = nfc.mifareclassic_WriteDataBlock (4, data);
 
-          uint8_t tempWrite[48] = {};
-          uint8_t tempWrite16[16] = {};
-          copy(begin(temp), end(temp), tempWrite);
-          copy_n(begin(tglByte), 4, begin(tempWrite) + 10);
-          copy_n(begin(tempWrite), 16, begin(tempWrite16));
-          nfc.PrintHexChar(tempWrite, 48);
-          nfc.PrintHexChar(tempWrite16, 16);
-          Serial.println("");
-
-
-          uint8_t nopol[10];
-          uint8_t tanggal[4];
-          uint8_t nip[18];
-          uint8_t expired[4];
-          uint8_t stMasuk;
-          uint8_t kodeGate;
-          uint8_t stKartu;
-
-          //          copy nopol
-          index = 0;
-          for (int i = 0; i < 10; i++) {
-            nopol[i] = temp[index++];
+          // Try to read the contents of block 4
+          uint8_t index16 = 0;
+          for (int i = 4; i < 7; i++) {
+            uint8_t data[16];
+            success = nfc.mifareclassic_ReadDataBlock(i, data);
+            copy_n(begin(data), 16, begin(temp) + index16);
+            index16 += 16;
           }
-          //copy tanggal
-          for (int i = 0; i < 4; i++) {
-            tanggal[i] = temp[index++];
+
+          if (success)
+          {
+            unsigned long unixTime = timeClient.getEpochTime();
+            Serial.println(unixTime);
+            String hexDate =  String(unixTime, HEX);
+            hexDate.toUpperCase();
+            Serial.println(hexDate);
+            char arrDate[hexDate.length() + 1];
+            hexDate.toCharArray(arrDate, hexDate.length() + 1);
+            uint8_t tglByte[4];
+            uint8_t tglByteTemp[4] = {};
+            hex2ByteArray(arrDate, 4, tglByte);
+            copy(begin(tglByte), end(tglByte), tglByteTemp);
+            nfc.PrintHexChar(tglByte, 4);
+            nfc.PrintHexChar(tglByteTemp, 4);
+            // Data seems to have been read ... spit it out
+            Serial.println("Reading Block 4:");
+            nfc.PrintHexChar(temp, 48);
+
+
+            uint8_t tempWrite[48] = {};
+            uint8_t tempWrite16[16] = {};
+            copy(begin(temp), end(temp), tempWrite);
+            copy_n(begin(tglByte), 4, begin(tempWrite) + 10);
+            copy_n(begin(tempWrite), 16, begin(tempWrite16));
+            nfc.PrintHexChar(tempWrite, 48);
+            nfc.PrintHexChar(tempWrite16, 16);
+            Serial.println("");
+
+
+            uint8_t nopol[10] = {};
+            uint8_t tanggal[4] = {};
+            uint8_t nip[18] = {};
+            uint8_t expired[4] = {};
+            uint8_t stMasuk = {};
+            uint8_t kodeGate = {};
+            uint8_t stKartu = {};
+
+            //          copy nopol
+            copy_n(begin(temp), 10, begin(nopol));
+            //copy tanggal
+            copy_n(begin(temp) + 10, 4, begin(tanggal));
+            stMasuk = temp[14];
+            kodeGate = temp[15];
+            //          copy nip
+            copy_n(begin(temp) + 16, 18, begin(nip));
+            //copy expired
+            copy_n(begin(temp) + 34, 4, begin(expired));
+            stKartu = temp[38];
+
+            Serial.print("Nopol: ");
+            nfc.PrintHexChar(nopol, 10);
+            String noKend = hex2Ascii(nopol, 10);
+            Serial.println(noKend);
+            Serial.print("Tanggal: " );
+            nfc.PrintHexChar(tanggal, 4);
+            Serial.println();
+            Serial.print("Nip: " );
+            nfc.PrintHexChar(nip, 18);
+            Serial.println();
+            Serial.print("Expired: " );
+            nfc.PrintHexChar(expired, 4);
+            Serial.println();
+            Serial.print("Status Masuk: " );
+            Serial.println(stMasuk);
+            Serial.print("Status Kartu: " );
+            Serial.println(stKartu);
+            Serial.print("Kode Gate: " );
+            Serial.println(kodeGate);
+            Serial.print("Tanggal long: " );
+            char str[8] = "";
+            array_to_string(tanggal, 4, str);
+            String tglTrx = String(str);
+            Serial.println(tglTrx);
+
+            uint64_t result = getUInt64fromHex(str);
+            unsigned long longTanggal = result;
+            Serial.println(String(longTanggal));
+            unsigned int add7Hour = 7 * 3600;
+            result += add7Hour;
+
+            char buff[32];
+            sprintf(buff, "%02d.%02d.%02d %02d:%02d:%02d", day(result), month(result), year(result), hour(result), minute(result), second(result));
+            Serial.println(buff);
+
+            char strExpired[8] = "";
+            array_to_string(expired, 4, strExpired);
+            String tglExpired = String(strExpired);
+            Serial.println(tglExpired);
+            uint64_t resultExpired = getUInt64fromHex(strExpired);
+
+            resultExpired += add7Hour;
+
+            char buffExpired[32];
+            sprintf(buffExpired, "%02d.%02d.%02d %02d:%02d:%02d", day(result), month(result), year(result), hour(result), minute(result), second(result));
+
+            printf(" bignum = %lld\n", result);
+            String tgl = uint64ToString(result);
+            Serial.println(tgl);
+            time_t ti = tgl.toInt();
+            char timeStringBuff[50]; //50 chars should be enough
+            strftime(timeStringBuff, sizeof(timeStringBuff), "%Y-%m-%d %H:%M:%S", localtime(&ti));
+            Serial.println(timeStringBuff);
+
+            //          unsigned long offset_days = 7;    // 7 hours
+            //          unsigned long t_unix_date1, t_unix_date2;
+            //          t_unix_date1 = result;
+            //          Serial.print("t_unix_date1: ");
+            //          Serial.println(t_unix_date1);
+            //          //          offset_days = offset_days * 86400;    // convert number of days to seconds
+            //          offset_days = 7 * 3600;
+            //          t_unix_date2 = result + offset_days;
+            //          Serial.print("t_unix_date2: ");
+            //          Serial.println(t_unix_date2);
+            //          printf("Date1: %4d-%02d-%02d %02d:%02d:%02d\n", year(t_unix_date1), month(t_unix_date1), day(t_unix_date1), hour(t_unix_date1), minute(t_unix_date1), second(t_unix_date1));
+            //          printf("Date2: %4d-%02d-%02d %02d:%02d:%02d\n", year(t_unix_date2), month(t_unix_date2), day(t_unix_date2), hour(t_unix_date2), minute(t_unix_date2), second(t_unix_date2));
+
+            if (!client.connected()) {
+              reconnect();
+            }
+            client.loop();
+
+            char strUid[8] = "";
+            array_to_string(uid, 4, strUid);
+
+            String stringSend = "";
+            String cardUid = String(strUid);
+            stringSend += cardUid;
+            stringSend += ";";
+
+            stringSend += noKend;
+            stringSend += ";";
+            stringSend += hexDate;
+            stringSend += ";";
+
+            String cardNip = hex2Ascii(nip, 18);
+            stringSend += cardNip;
+            stringSend += ";";
+
+            stringSend += tglExpired;
+            stringSend += ";";
+
+            String cardStMasuk = String(stMasuk);
+            stringSend += cardStMasuk;
+            stringSend += ";";
+
+            String cardStKartu = String(stKartu);
+            stringSend += cardStKartu;
+            stringSend += ";";
+
+            String cardKodeGate = String(kodeGate);
+            stringSend += cardKodeGate;
+
+            success = nfc.mifareclassic_WriteDataBlock (4, tempWrite16);
+            if (success) {
+              Serial.println(stringSend);
+              char publish[stringSend.length() + 1];
+              stringSend.toCharArray(publish, stringSend.length() + 1);
+              client.publish("parkir/card", publish);
+
+              // Wait a bit before reading the card again
+              delay(1000);
+            } else {
+              Serial.println("Write block 4 gagal...");
+            }
           }
-          stMasuk = temp[index++];
-          kodeGate = temp[index++];
-          //          copy nip
-          for (int i = 0; i < 18; i++) {
-            nip[i] = temp[index++];
-          }
-          //copy expired
-          for (int i = 0; i < 4; i++) {
-            expired[i] = temp[index++];
-          }
-          stKartu = temp[index];
-
-          Serial.print("Nopol: ");
-          nfc.PrintHexChar(nopol, 10);
-          String noKend = hex2Ascii(nopol, 10);
-          Serial.println(noKend);
-          Serial.print("Tanggal: " );
-          nfc.PrintHexChar(tanggal, 4);
-          Serial.println();
-          Serial.print("Nip: " );
-          nfc.PrintHexChar(nip, 18);
-          Serial.println();
-          Serial.print("Expired: " );
-          nfc.PrintHexChar(expired, 4);
-          Serial.println();
-          Serial.print("Status Masuk: " );
-          Serial.println(stMasuk);
-          Serial.print("Status Kartu: " );
-          Serial.println(stKartu);
-          Serial.print("Kode Gate: " );
-          Serial.println(kodeGate);
-          Serial.print("Tanggal long: " );
-          char str[8] = "";
-          array_to_string(tanggal, 4, str);
-          String tglTrx = String(str);
-          Serial.println(tglTrx);
-
-          time_t now;
-
-          time(&now);
-          Serial.println(now);
-
-
-          uint64_t result = getUInt64fromHex(str);
-          unsigned long longTanggal = result;
-          Serial.println(String(longTanggal));
-          unsigned int add7Hour = 7 * 3600;
-          result += add7Hour;
-
-          char buff[32];
-          sprintf(buff, "%02d.%02d.%02d %02d:%02d:%02d", day(result), month(result), year(result), hour(result), minute(result), second(result));
-          Serial.println(buff);
-
-          char strExpired[8] = "";
-          array_to_string(expired, 4, strExpired);
-          String tglExpired = String(strExpired);
-          Serial.println(tglExpired);
-          uint64_t resultExpired = getUInt64fromHex(strExpired);
-
-          resultExpired += add7Hour;
-
-          char buffExpired[32];
-          sprintf(buffExpired, "%02d.%02d.%02d %02d:%02d:%02d", day(result), month(result), year(result), hour(result), minute(result), second(result));
-
-          printf(" bignum = %lld\n", result);
-          String tgl = uint64ToString(result);
-          Serial.println(tgl);
-          time_t ti = tgl.toInt();
-          char timeStringBuff[50]; //50 chars should be enough
-          strftime(timeStringBuff, sizeof(timeStringBuff), "%Y-%m-%d %H:%M:%S", localtime(&ti));
-          Serial.println(timeStringBuff);
-
-          //          unsigned long offset_days = 7;    // 7 hours
-          //          unsigned long t_unix_date1, t_unix_date2;
-          //          t_unix_date1 = result;
-          //          Serial.print("t_unix_date1: ");
-          //          Serial.println(t_unix_date1);
-          //          //          offset_days = offset_days * 86400;    // convert number of days to seconds
-          //          offset_days = 7 * 3600;
-          //          t_unix_date2 = result + offset_days;
-          //          Serial.print("t_unix_date2: ");
-          //          Serial.println(t_unix_date2);
-          //          printf("Date1: %4d-%02d-%02d %02d:%02d:%02d\n", year(t_unix_date1), month(t_unix_date1), day(t_unix_date1), hour(t_unix_date1), minute(t_unix_date1), second(t_unix_date1));
-          //          printf("Date2: %4d-%02d-%02d %02d:%02d:%02d\n", year(t_unix_date2), month(t_unix_date2), day(t_unix_date2), hour(t_unix_date2), minute(t_unix_date2), second(t_unix_date2));
-
-          if (!client.connected()) {
-            reconnect();
-          }
-          client.loop();
-
-          char strUid[8] = "";
-          array_to_string(uid, 4, strUid);
-
-          String stringSend = "";
-          String cardUid = String(strUid);
-          stringSend += cardUid;
-          stringSend += ";";
-
-          stringSend += noKend;
-          stringSend += ";";
-          stringSend += hexDate;
-          stringSend += ";";
-
-          String cardNip = hex2Ascii(nip, 18);
-          stringSend += cardNip;
-          stringSend += ";";
-
-          stringSend += tglExpired;
-          stringSend += ";";
-
-          String cardStMasuk = String(stMasuk);
-          stringSend += cardStMasuk;
-          stringSend += ";";
-
-          String cardStKartu = String(stKartu);
-          stringSend += cardStKartu;
-          stringSend += ";";
-
-          String cardKodeGate = String(kodeGate);
-          stringSend += cardKodeGate;
-
-          success = nfc.mifareclassic_WriteDataBlock (4, tempWrite16);
-          if (success) {
-            Serial.println(stringSend);
-            char publish[stringSend.length() + 1];
-            stringSend.toCharArray(publish, stringSend.length() + 1);
-            client.publish("parkir/card", publish);
-
-            // Wait a bit before reading the card again
-            delay(1000);
-          } else {
-            Serial.println("Write block 4 gagal...");
+          else
+          {
+            Serial.println("Ooops ... unable to read the requested block.  Try another key?");
           }
         }
         else
         {
-          Serial.println("Ooops ... unable to read the requested block.  Try another key?");
+          Serial.println("Ooops ... authentication failed: Try another key?");
         }
       }
-      else
-      {
-        Serial.println("Ooops ... authentication failed: Try another key?");
-      }
+      delay(2000);
     }
-    delay(2000);
+    serialNumber = cardSerial;
+  } else {
+    serialNumber = "";
   }
 }
 
